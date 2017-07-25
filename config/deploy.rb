3d17c01de49225c8113f9437f26287e4add7886a
Thomas Eisenbarth <thomas.eisenbarth@makandra.de>
2017-07-25 18:24:42 +0200

    set some defaults for our environment

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,12 +6,14 @@ set :scm, :git
 set :repo_url, 'git@github.com:makandra/frab.git'
 
 set :branch, 'master'
-set :user, ENV['CAP_USER']
+
+set :keep_releases, 10
+set :ssh_options, {
+    forward_agent: true
+}
 
 set :use_sudo,        false
 set :stage,           :production
-set :deploy_via,      :remote_cache
-set :ssh_options,     forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub)
 set :bundle_without, %w(capistrano development test sqlite3).join(' ')
-set :linked_files, %w(config/database.yml .env.production .ruby-version)
-set :linked_dirs,  %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
+set :linked_files, %w(config/database.yml)
+set :linked_dirs, %w(log public/system)
