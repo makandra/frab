bff18c23974ac93bb41e89a5218f788687b54cd1
Thomas Eisenbarth <thomas.eisenbarth@makandra.de>
2017-07-25 18:21:48 +0200

    bundle postgres when deploying

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,6 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :ssh_options,     forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub)
-set :bundle_without, %w(capistrano development test postgresql sqlite3).join(' ')
+set :bundle_without, %w(capistrano development test sqlite3).join(' ')
 set :linked_files, %w(config/database.yml .env.production .ruby-version)
 set :linked_dirs,  %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
